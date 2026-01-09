11e5fc2fd0da88b4f822d87f0dcddb96d92fbbc2
Matheus Benatti <matheus.cwb2@gmail.com>
2026-01-08 22:30:58 -0300

    ISS Overhead Project

---
+++ Project33ISSOverhead/main.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime
+import time
+
+MY_LAT = 1 # Your latitude
+MY_LONG = 1 # Your longitude
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunrise or time_now <= sunset:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_night() and is_iss_overhead():
+        print("Look up")
+    else:
+        print("Wait")
