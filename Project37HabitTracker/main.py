0fc7265b50db1706367a099599fc6d081e7262eb
Matheus Benatti <matheus.cwb2@gmail.com>
2026-01-19 21:10:47 -0300

    HabitTracker

---
+++ Project37HabitTracker/main.py
@@ -0,0 +1,58 @@
+import requests
+from datetime import datetime
+
+pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "matheusbenatti"
+TOKEN = ""
+GRAPH_ID = "graph1"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Study Graph",
+    "unit": "Minutes",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": int(input("How many minutes did you study today?")),
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "40"
+}
+
+#response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
